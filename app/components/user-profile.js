ae8fd77a1a7305f2f3f87e4a5559b0baa21803de
Radosław Nowacki <radoslaw.nowacki@blstream.com>
2016-10-27 21:10:20 +0200

    working solution ember

---
+++ app/components/user-profile.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'tr',
+  actions: {
+    delete(id) {
+      this.get('delete')(id);
+    },
+
+    close() {
+      this.set('isActive', false);
+    },
+
+    open() {
+      this.set('isActive', true)
+    },
+
+    updateModel(label) {
+      const user = this.get('user');
+      user.set(label, 'dupa')
+    }
+  },
+
+  init() {
+    this._super(...arguments);
+    const user = this.get('user');
+    this.userId = this.get('user.id');
+    this.set('isActive', false);
+    this.set('labels', Object.keys(user.toJSON()));
+  }
+});
